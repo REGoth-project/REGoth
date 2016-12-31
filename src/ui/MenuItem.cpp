888527111fd969ed8d581551eb20b83f9911e0c3
Andre Taulien <stu114706@uni-kiel.de>
2016-12-31 19:38:42 +0100

    Selectable items and item background images

--- src/ui/MenuItem.cpp
+++ src/ui/MenuItem.cpp
@@ -4,8 +4,10 @@
 
 #include "MenuItem.h"
 #include "Menu.h"
+#include "ImageView.h"
 #include "zFont.h"
 #include <engine/BaseEngine.h>
+#include <utils/logger.h>
 
 using namespace UI;
 
@@ -22,13 +24,36 @@ MenuItem::MenuItem(Engine::BaseEngine& e, Menu& baseMenu, Daedalus::GameState::M
 
     if((item.flags & Daedalus::GEngineClasses::C_Menu_Item::IT_TXT_CENTER) != 0)
     {
-        setTranslation(m_Translation * 0.5f + (m_Translation + m_Size) * 0.5f);
-        setAlignment(A_Center);
+        //setTranslation(m_Translation * 0.5f + (m_Translation + m_Size) * 0.5f);
+        //setAlignment(A_Center);
     }
+
+    m_pBackgroundImage = nullptr;
+    if(!item.backPic.empty())
+    {
+        m_pBackgroundImage = new ImageView(m_Engine);
+        // m_pBackgroundImage->setTranslation(Math::float2(item.posx / 8192.0f, item.posy / 8192.0f));
+        // m_pBackgroundImage->setSize(Math::float2(item.dimx / 8192.0f, item.dimy / 8192.0f));
+
+        Handle::TextureHandle bgr = m_Engine.getEngineTextureAlloc().loadTextureVDF(getItemScriptData().backPic);
+        m_pBackgroundImage->setImage(m_Engine.getEngineTextureAlloc().getTexture(bgr).m_TextureHandle);
+        m_pBackgroundImage->setRelativeSize(false); // Scale like this screen
+        m_pBackgroundImage->setAlignment(A_Center);
+        addChild(m_pBackgroundImage);
+    }
+}
+
+MenuItem::~MenuItem()
+{
+    removeChild(m_pBackgroundImage);
+    delete m_pBackgroundImage;
 }
 
 void MenuItem::update(double dt, Engine::Input::MouseState& mstate, Render::RenderConfig& config)
 {
+    if(m_pBackgroundImage) // Background image shouldn't be centered in any way
+        m_pBackgroundImage->setAlignment(A_TopLeft);
+
     View::update(dt, mstate, config);
 
     if(!m_ScriptHandle.isValid())
@@ -90,11 +115,40 @@ void MenuItemTypes::MenuItemText::update(double dt, Engine::Input::MouseState& m
     int px = (int) (absTranslation.x * config.state.viewWidth + 0.5f);
     int py = (int) (absTranslation.y * config.state.viewHeight + 0.5f);
 
-    for(unsigned int i=0;i<Daedalus::GEngineClasses::MenuConstants::MAX_USERSTRINGS; i++)
+    if(!item.text[0].empty())
     {
-        if(!item.text[i].empty())
-        {
-            drawText(item.text[i], px, py + fnt->getFontHeight() * i, m_Alignment, config, item.fontName);
-        }
+         
+        int sx = (int) (absSize.x * config.state.viewWidth + 0.5f);
+        int sy = (int) (absSize.y * config.state.viewHeight + 0.5f);
+
+        px += sx / 2;
+        py += sy / 2;
+
+        EAlign align = (item.flags & Daedalus::GEngineClasses::C_Menu_Item::IT_TXT_CENTER) != 0 ? A_Center : A_TopLeft;
+        drawText(item.text[0], px, py, align, config, item.fontName);
     }
+}
+
+
+void MenuItem::setFontHighlighted(bool value)
+{
+    std::string fontBase = getItemScriptData().fontName; 
+
+    fontBase = Utils::stripExtension(fontBase);
+
+    // Strip the _HI, if present
+    if(fontBase.substr(fontBase.size() - 3) == "_HI")
+        fontBase = fontBase.substr(0, fontBase.size() - 3);
+
+    if(value)
+        fontBase += "_HI";
+
+    getItemScriptData().fontName = fontBase;
+}
+
+
+Daedalus::GEngineClasses::MenuConstants::ESelEvent MenuItem::getSelectionEvent(int i)
+{ 
+    assert(i < Daedalus::GEngineClasses::MenuConstants::MAX_SEL_ACTIONS);
+    return (Daedalus::GEngineClasses::MenuConstants::ESelEvent)getItemScriptData().onSelAction[i]; 
 }
