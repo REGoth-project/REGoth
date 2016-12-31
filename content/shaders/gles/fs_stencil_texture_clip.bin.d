3ca0ad7ef6b6f70afd81db41b9b078a49b0d9796
Andre Taulien <stu114706@uni-kiel.de>
2017-01-01 00:35:43 +0100

    Fixed some missing/broken shaders

--- content/shaders/gles/fs_stencil_texture_clip.bin.d
+++ content/shaders/gles/fs_stencil_texture_clip.bin.d
@@ -1,2 +1,6 @@
 ../../.build/shaders/gles/fs_stencil_texture_clip.bin :  \
- varying.def.sc
+ varying.def.sc \
+ ../common/common.sh \
+ ../../scripts//../src/bgfx_shader.sh \
+ ../common/shaderlib.sh \
+ tools.sh
