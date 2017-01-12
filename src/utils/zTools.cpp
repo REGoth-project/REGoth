e1e472c16afe438b5ef51aa63d72895cf29cf5d4
Andre Taulien <a.taulien@live.de>
2017-01-12 18:11:57 +0100

    CLI: Place to put helper-function to run from CLI

---
+++ src/utils/zTools.cpp
@@ -0,0 +1,2 @@
+#include "zTools.h"
+
---
+++ src/utils/zTools.h
@@ -0,0 +1,15 @@
+//
+// Created by desktop on 12.01.17.
+//
+
+#pragma once
+
+/**
+ * Commandline tools for specific tasks regarding Gothic
+ */
+namespace zTools
+{
+
+}
+
+
