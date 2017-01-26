c15db4576d0f454aa40badfc12d01d2e5bd60843
Andre Taulien <a.taulien@live.de>
2017-01-26 18:42:34 +0100

    Engine: Enabled rendering to other views + viewconfig

---
+++ src/render/ViewList.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <inttypes.h>
+
+/**
+ * Explicit drawing-orders or views used for special effects must be set here
+ */
+
+namespace RenderViewList
+{
+    const uint8_t DEFAULT = 0; // Opaque
+    const uint8_t ALPHA_1 = 1; // Draws smoke, etc
+    const uint8_t ALPHA_2 = 2; // Draws fire above the smoke
+    const uint8_t UI = 254;
+}
