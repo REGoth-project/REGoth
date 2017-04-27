520ad8216c83700164c53ebfd4125c923409439e
Nico Bendlin <nico@nicode.net>
2017-04-27 09:18:53 +0200

    documented G1 game externals
    Added documentation for AI_LookForItem (only available in G1).

---
+++ src/logic/scriptExternals/externalsGenerator/externals.game.g1.d
@@ -0,0 +1,20 @@
+///
+/// \weakgroup parser_game ZenGin GAME parser externals.
+/// @{
+///
+
+//#define C_ITEM_ID int
+
+FUNC void AI_LookForItem(VAR C_NPC Character, VAR C_ITEM_ID ItemId)
+/// \brief      Not implemented.
+/// \param      Character
+///                 Reserved. Reference to a character object.
+/// \param      ItemId
+///                 Reserved. Index of a C_Item parser symbol.
+/// \note       The function is only available in G1.
+{
+};
+
+///
+/// @}
+///
