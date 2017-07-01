4554f5d5fee116615572ba28d71ee2348a3650d1
Michael Langfermann <versable@gmail.com>
2017-07-01 12:09:53 +0200

    Add header for REGoth class

---
+++ src/target/REGoth.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "bgfx_utils.h"
+#include <utils/bgfx_lib.h>
+#include <engine/World.h>
+#include <engine/GameEngine.h>
+
+#if BX_PLATFORM_ANDROID
+#include "engine/PlatformAndroid.h"
+#define PLATFORM_CLASS Engine::PlatformAndroid
+#elif BX_PLATFORM_LINUX || BX_PLATFORM_OSX || BX_PLATFORM_WINDOWS || BX_PLATFORM_EMSCRIPTEN
+#include "engine/PlatformGLFW.h"
+#define PLATFORM_CLASS Engine::PlatformGLFW
+#else
+#error Unknown platform
+#endif
+
+class REGoth : public PLATFORM_CLASS
+{
+    public:
+        void renderScreenSpaceQuad(uint32_t _view, bgfx::ProgramHandle _program, float _x, float _y, float _width, float _height);
+        void drawLog();
+        void showSplash();
+        void init(int _argc, char** _argv) BX_OVERRIDE;
+        int shutdown() override;
+
+        bool update() BX_OVERRIDE;
+
+        Engine::GameEngine* m_pEngine;
+        uint32_t m_debug;
+        uint32_t m_reset;
+        int m_Width, m_Height;
+        int64_t m_timeOffset;
+        float axis;
+        int32_t m_scrollArea;
+        bool m_NoHUD;
+        // prevents imgui from crashing if we failed on startup and didn't init it
+        bool m_ImgUiCreated = false;
+};
+
+
