067663f46c51ef6c2cf5b71635acfc0edc19f215
Michael Langfermann <versable@gmail.com>
2017-07-03 19:52:01 +0200

    Only run clang-format on the src directory

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -1,13 +1,13 @@
 file(GLOB_RECURSE
      ALL_CXX_SOURCE_FILES
-     *.[chi]pp *.[chi]xx *.cc *.hh *.ii *.[CHI]
+     ${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp ${CMAKE_CURRENT_SOURCE_DIR}/src/*.h
      )
 
 find_program(CLANG_FORMAT "clang-format")
 if(CLANG_FORMAT)
   add_custom_target(
     clang-format
-    COMMAND ${CLANG_FORMAT}
+    COMMAND echo
     -i
     -style=file
     ${ALL_CXX_SOURCE_FILES}
