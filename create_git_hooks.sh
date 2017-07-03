597f2889f528930ba5564940cfbc39788302b50b
Michael Langfermann <versable@gmail.com>
2017-07-03 19:28:50 +0200

    Add git hook and bash script for easy hook symlinking

---
+++ create_git_hooks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+GIT_REPO_DIR=$(git rev-parse --show-toplevel)
+
+cd "$GIT_REPO_DIR"
+
+for hook in hooks/*
+do
+    SYMLINK_HOOK=.git/$hook
+    if [ -f $hook ] && [ ! -e "$SYMLINK_HOOK" ] && [ ! -L "$SYMLINK_HOOK" ]
+    then
+        eval "ln -s $hook .git/hooks/"
+    fi
+done
