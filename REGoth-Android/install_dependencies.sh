868adf8ac4673b104d5da0b8d7c61c53591ec871
Andre Taulien <a.taulien@live.de>
2017-09-26 09:04:23 +0200

    Travis: Android SDK installation

--- REGoth-Android/install_dependencies.sh
+++ REGoth-Android/install_dependencies.sh
@@ -5,8 +5,8 @@ mkdir -p android-dependencies
 pushd android-dependencies
 
 # Download Android SDK and NDK
-curl -L https://dl.google.com/android/repository/android-ndk-r14b-linux-x86_64.zip -O
-curl -L https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip -O
+#curl -L https://dl.google.com/android/repository/android-ndk-r14b-linux-x86_64.zip -O
+#curl -L https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip -O
 
 
 # Install NDK
@@ -14,16 +14,28 @@ unzip -q android-ndk-r14b-linux-x86_64.zip
 rm android-ndk-r14b-linux-x86_64.zip
 
 # Install SDK
-unzip -q sdk-tools-linux-3859397.zip
+mkdir -p android-sdk
+unzip -q sdk-tools-linux-3859397.zip -d android-sdk
 rm sdk-tools-linux-3859397.zip
 
 # Setup environment
-export ANDROID_HOME=`pwd`/sdk-tools-linux-3859397
+export ANDROID_HOME=`pwd`/android-sdk
 export ANDROID_NDK_HOME=`pwd`/android-ndk-r14b-linux-x86_64
 export LOCAL_ANDROID_NDK_HOME="$ANDROID_NDK_HOME"
 export LOCAL_ANDROID_NDK_HOST_PLATFORM="linux-x86_64"
-export PATH=$PATH:${ANDROID_SDK_HOME}
+export PATH=$PATH:${ANDROID_HOME}
+export PATH=$PATH:${ANDROID_HOME}/tools
 export PATH=$PATH:${ANDROID_NDK_HOME}
+
+# Setup Android SDK
+echo "Andorid SDK Licenses..."
+yes | ${ANDROID_HOME}/tools/bin/sdkmanager --licenses
+
+echo "Updating SDK"
+yes | ${ANDROID_HOME}/tools/bin/sdkmanager --update
+
+echo "Downloading Android Build Tools"
+yes | ${ANDROID_HOME}/tools/bin/sdkmanager "platforms;android-21" "build-tools;25.0.2" "extras;google;m2repository" "extras;android;m2repository"
 
 popd # Leaving dependencies
 popd # Leaving script-folder
