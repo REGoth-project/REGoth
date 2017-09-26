a08588f0076e800948528192e6663c1b08b20fcf
Andre Taulien <a.taulien@live.de>
2017-09-26 13:02:37 +0200

    Travis: Build actually failing when internal script failed + getting submodules now

--- .travis_build.sh
+++ .travis_build.sh
@@ -6,6 +6,7 @@ set -o pipefail
 
 if [ "$PLATFORM" = "android" ]; then
     REGoth-Android/install_dependencies_and_build.sh;
+    [[ $? -ne 0 ]] && exit 1; # Exit if non-zero exit code
 else
     if [ "$CXX" = "g++" ]; then export CXX="g++-6" CC="gcc-6"; fi
     git submodule update --init --recursive;
--- REGoth-Android/install_dependencies_and_build.sh
+++ REGoth-Android/install_dependencies_and_build.sh
@@ -1,3 +1,5 @@
+git submodule update --init --recursive
+
 pushd "$(dirname "$0")"
 
 mkdir -p android-dependencies
