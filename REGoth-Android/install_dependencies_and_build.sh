01e0ae27d5b6175319b1308c2be76f25a12e8637
Andre Taulien <a.taulien@live.de>
2017-09-26 09:38:16 +0200

    Travis: Using the right ndk-folder

--- REGoth-Android/install_dependencies_and_build.sh
+++ REGoth-Android/install_dependencies_and_build.sh
@@ -10,7 +10,8 @@ curl -L https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip -O
 
 
 # Install NDK
-unzip -q android-ndk-r14b-linux-x86_64.zip
+mkdir -p android-ndk
+unzip -q android-ndk-r14b-linux-x86_64.zip -d android-ndk
 rm android-ndk-r14b-linux-x86_64.zip
 
 # Install SDK
@@ -20,7 +21,7 @@ rm sdk-tools-linux-3859397.zip
 
 # Setup environment
 export ANDROID_HOME=`pwd`/android-sdk
-export ANDROID_NDK_HOME=`pwd`/android-ndk-r14b-linux-x86_64
+export ANDROID_NDK_HOME=`pwd`/android-ndk
 export LOCAL_ANDROID_NDK_HOME="$ANDROID_NDK_HOME"
 export LOCAL_ANDROID_NDK_HOST_PLATFORM="linux-x86_64"
 export PATH=$PATH:${ANDROID_HOME}
