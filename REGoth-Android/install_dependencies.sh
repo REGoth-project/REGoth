93ab5ed5426e07bfb1819a44c2a2817b80bcf62d
Andre Taulien <a.taulien@live.de>
2017-09-26 08:47:38 +0200

    Travis: Using right android sdk tools now

--- REGoth-Android/install_dependencies.sh
+++ REGoth-Android/install_dependencies.sh
@@ -6,7 +6,7 @@ pushd android-dependencies
 
 # Download Android SDK and NDK
 curl -L https://dl.google.com/android/repository/android-ndk-r14b-linux-x86_64.zip -O
-curl -L https://dl.google.com/dl/android/studio/ide-zips/2.3.3.0/android-studio-ide-162.4069837-linux.zip -O
+curl -L https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip -O
 
 
 # Install NDK
@@ -14,11 +14,11 @@ unzip -q android-ndk-r14b-linux-x86_64.zip
 rm android-ndk-r14b-linux-x86_64.zip
 
 # Install SDK
-unzip -q android-studio-ide-162.4069837-linux.zip
-rm android-studio-ide-162.4069837-linux.zip
+unzip -q sdk-tools-linux-3859397.zip
+rm sdk-tools-linux-3859397.zip
 
 # Setup environment
-export ANDROID_HOME=`pwd`/android-studio-ide-162.4069837-linux
+export ANDROID_HOME=`pwd`/sdk-tools-linux-3859397
 export ANDROID_NDK_HOME=`pwd`/android-ndk-r14b-linux-x86_64
 export LOCAL_ANDROID_NDK_HOME="$ANDROID_NDK_HOME"
 export LOCAL_ANDROID_NDK_HOST_PLATFORM="linux-x86_64"
