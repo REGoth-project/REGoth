6a4fb04d5e7ecd4f98d1d5c398c20d8b3344e752
markusobi <halbesbyte@googlemail.com>
2017-10-09 00:49:01 +0200

    disabled LMB. PlayerAction no longer fires multiple times

--- src/engine/PlatformGLFW.cpp
+++ src/engine/PlatformGLFW.cpp
@@ -214,7 +214,8 @@ int32_t PlatformGLFW::run(int argc, char** argv)
     bindKey(GLFW_KEY_SPACE, ActionType::DebugMoveSpeed, true);
     bindKey(GLFW_KEY_LEFT_SHIFT, ActionType::DebugMoveSpeed2, true);
     bindKey(GLFW_KEY_LEFT_CONTROL, ActionType::PlayerAction, true);
-    bindMouseButton(GLFW_MOUSE_BUTTON_LEFT, ActionType::PlayerAction, true);
+    // mouse button is currently bugged. causing PlayerAction to fire multiple times even from LEFT_CONTROL
+    //bindMouseButton(GLFW_MOUSE_BUTTON_LEFT, ActionType::PlayerAction, true);
     bindMouseAxis(MouseAxis::CursorX, ActionType::PlayerRotate, true, false);
 
     bindKey(GLFW_KEY_F5, ActionType::Quicksave, false);
