720cae009315766064d4de08849f5e598c6bbdf8
Sergey Kukunin <sergey.kukunin@gmail.com>
2017-10-09 01:56:41 +0300

    Use native android Travis image

--- .travis_build.sh
+++ .travis_build.sh
@@ -5,11 +5,10 @@ set -e
 set -o pipefail
 
 if [ "$PLATFORM" = "android" ]; then
-    REGoth-Android/install_dependencies_and_build.sh;
-    [[ $? -ne 0 ]] && exit 1; # Exit if non-zero exit code
+    cd REGoth-Android/
+    ./gradlew build
 else
     if [ "$CXX" = "g++" ]; then export CXX="g++-6" CC="gcc-6"; fi
-    git submodule update --init --recursive;
     mkdir -p build;
     cd build;
     cmake ..;
