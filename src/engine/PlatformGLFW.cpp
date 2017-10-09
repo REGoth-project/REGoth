631a76748d6f9e5841e545bcba51a0907375d420
Alexander Eberharter <eberharter.alexander@gmail.com>
2017-10-09 16:40:51 +0200

    Implemented basic inventory viewer. Some items don't render properly and so far there are no interactions possible. fixed inventory commit added all files

--- src/engine/PlatformGLFW.cpp
+++ src/engine/PlatformGLFW.cpp
@@ -217,7 +217,10 @@ int32_t PlatformGLFW::run(int argc, char** argv)
     bindKey(GLFW_KEY_LEFT_CONTROL, ActionType::PlayerActionContinous, true);
     bindMouseButton(GLFW_MOUSE_BUTTON_LEFT, ActionType::PlayerAction, false);
     bindMouseButton(GLFW_MOUSE_BUTTON_LEFT, ActionType::PlayerActionContinous, true);
+    bindKey(GLFW_KEY_LEFT_CONTROL, ActionType::PlayerAction, true);
     bindMouseAxis(MouseAxis::CursorX, ActionType::PlayerRotate, true, false);
+
+    bindKey(GLFW_KEY_TAB, ActionType::HUD_ShowInventory, false);
 
     bindKey(GLFW_KEY_F5, ActionType::Quicksave, false);
     bindKey(GLFW_KEY_F9, ActionType::Quickload, false);
