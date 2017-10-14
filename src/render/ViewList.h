237832da044c72fe38e36bf4ad8d286ee646c887
Andre Taulien <a.taulien@live.de>
2017-10-14 19:54:01 +0200

    Renderer: Added VERY rough and nasty rendering of the skydome of G2

--- src/render/ViewList.h
+++ src/render/ViewList.h
@@ -7,8 +7,9 @@
 
 namespace RenderViewList
 {
-    const uint8_t DEFAULT = 0;  // Opaque
-    const uint8_t ALPHA_1 = 1;  // Draws smoke, etc
-    const uint8_t ALPHA_2 = 2;  // Draws fire above the smoke
+    const uint8_t PRE_WORLD_SKY = 0;
+    const uint8_t DEFAULT = 1;  // Opaque
+    const uint8_t ALPHA_1 = 2;  // Draws smoke, etc
+    const uint8_t ALPHA_2 = 3;  // Draws fire above the smoke
     const uint8_t UI = 254;
 }
