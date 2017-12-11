077ec110e03cee40bd51b80b1b8eed91d92d13b6
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2017-12-11 22:55:17 +0100

    Make libsndfile a separate compilation step

--- bin/build_unix.sh
+++ bin/build_unix.sh
@@ -2,7 +2,14 @@
 set -e -x -o pipefail
 
 if [ "$CXX" = "g++" ]; then export CXX="g++-6" CC="gcc-6"; fi
+
+mkdir -p build-libsndfile
+mkdir -p $TRAVIS_BUILD_DIR/sndfile_install
+cd build-libsndfile
+cmake -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR/sndfile_install ../lib/libdmusic/utils/dls2sf/lib/libsndfile
+cd ../
+
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$TRAVIS_BUILD_DIR/sndfile_install ..
 make -j4
