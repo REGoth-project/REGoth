aa7c2c734af808154ff49d0abd1d4197a8777b39
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2017-12-12 12:47:00 +0100

    Use system-installed libsndfile libaries on *nix

--- bin/build_unix.sh
+++ bin/build_unix.sh
@@ -3,14 +3,7 @@ set -e -x -o pipefail
 
 if [ "$CXX" = "g++" ]; then export CXX="g++-6" CC="gcc-6"; fi
 
-mkdir -p build-libsndfile
-mkdir -p $TRAVIS_BUILD_DIR/sndfile_install
-cd build-libsndfile
-cmake -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR/sndfile_install ../lib/libdmusic/utils/dls2sf/lib/libsndfile
-make -j4
-cd ../
-
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$TRAVIS_BUILD_DIR/sndfile_install ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j4
