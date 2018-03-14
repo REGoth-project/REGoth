1c5965a53534653b44affcb9547a161ab9e3fa33
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2018-03-14 19:05:53 +0100

    Update libdmusic & try fix Android build

--- bin/build_android.sh
+++ bin/build_android.sh
@@ -3,12 +3,13 @@ set -e -x -o pipefail
 
 export TERM=dumb # Fixes gradle output looking bad
 
-cd lib/libdmusic/utils/dls2sf/lib/libsndfile
-./autogen.sh
-./Scripts/android-configure.sh
+mkdir build-libsndfile
+cd build-libsndfile
+
+cmake -DCMAKE_SYSTEM_NAME=Android -DANDROID_NDK=$ANDROID_NDK_HOME -DDISABLE_EXTERNAL_LIBS=On -DCMAKE_INSTALL_PREFIX=/opt/ ../lib/libdmusic/utils/dls2sf/lib/libsndfile
 make -j4
 make install
 
-cd ../../../../../../REGoth-Android/
+cd ../REGoth-Android/
 # Release build should be signed, while debug signs automatically
 ./gradlew assembleDebug
