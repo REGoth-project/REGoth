beb6bdbf288a07f53d13416df354e03da6b58270
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2018-04-19 17:37:51 +0200

    Fix android compilation

--- bin/build_android.sh
+++ bin/build_android.sh
@@ -6,7 +6,7 @@ export TERM=dumb # Fixes gradle output looking bad
 mkdir build-libsndfile
 cd build-libsndfile
 
-cmake -DCMAKE_SYSTEM_NAME=Android -DANDROID_NDK=$ANDROID_NDK_HOME -DDISABLE_EXTERNAL_LIBS=On -DCMAKE_INSTALL_PREFIX=/opt/ ../lib/libdmusic/utils/dls2sf/lib/libsndfile
+cmake -DCMAKE_SYSTEM_NAME=Android -DANDROID_NDK=$ANDROID_NDK_HOME -DDISABLE_EXTERNAL_LIBS=On -DCMAKE_INSTALL_PREFIX=$ANDROID_NDK_HOME/sysroot ../lib/libdmusic/utils/dls2sf/lib/libsndfile
 make -j4
 make install
 
