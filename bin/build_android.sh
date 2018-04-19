9dae8a0e5f10baca63a67ce12699d6599b968a54
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2018-04-19 22:14:38 +0200

    Disable building libsndfile utilities on android

--- bin/build_android.sh
+++ bin/build_android.sh
@@ -6,7 +6,9 @@ export TERM=dumb # Fixes gradle output looking bad
 mkdir build-libsndfile
 cd build-libsndfile
 
-cmake -DCMAKE_SYSTEM_NAME=Android -DANDROID_NDK=$ANDROID_NDK_HOME -DDISABLE_EXTERNAL_LIBS=On -DCMAKE_INSTALL_PREFIX=$ANDROID_NDK_HOME/sysroot ../lib/libdmusic/utils/dls2sf/lib/libsndfile
+cmake -DCMAKE_SYSTEM_NAME=Android -DBUILD_PROGRAMS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF \
+        -DANDROID_NDK=$ANDROID_NDK_HOME -DDISABLE_EXTERNAL_LIBS=On -DCMAKE_INSTALL_PREFIX=$ANDROID_NDK_HOME/sysroot \
+        ../lib/libdmusic/utils/dls2sf/lib/libsndfile
 make -j4
 make install
 
