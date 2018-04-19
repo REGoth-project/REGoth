b582c666f0c19c4b0649fd018c76185ed55048c7
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2018-04-19 22:23:49 +0200

    libsndfile: disable build regtest

--- bin/build_android.sh
+++ bin/build_android.sh
@@ -7,8 +7,8 @@ mkdir build-libsndfile
 cd build-libsndfile
 
 cmake -DCMAKE_SYSTEM_NAME=Android -DBUILD_PROGRAMS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF \
-        -DANDROID_NDK=$ANDROID_NDK_HOME -DDISABLE_EXTERNAL_LIBS=On -DCMAKE_INSTALL_PREFIX=$ANDROID_NDK_HOME/sysroot \
-        ../lib/libdmusic/utils/dls2sf/lib/libsndfile
+        -DBUILD_REGTEST=OFF -DANDROID_NDK=$ANDROID_NDK_HOME -DDISABLE_EXTERNAL_LIBS=On \
+        -DCMAKE_INSTALL_PREFIX=$ANDROID_NDK_HOME/sysroot ../lib/libdmusic/utils/dls2sf/lib/libsndfile
 make -j4
 make install
 
