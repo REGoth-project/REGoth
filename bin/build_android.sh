6d0f8c1d17e0840a792185ad82a1d28062d69a05
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2018-04-23 12:24:50 +0200

    Correct Android building to make it compile with newer Gradle versions

--- bin/build_android.sh
+++ bin/build_android.sh
@@ -12,6 +12,9 @@ cmake -DCMAKE_SYSTEM_NAME=Android -DBUILD_PROGRAMS=OFF -DBUILD_EXAMPLES=OFF -DBU
 make -j4
 make install
 
+mkdir -p ../REGoth-Android/app/src/main/jniLibs/armeabi-v7a
+cp $ANDROID_NDK_HOME/sysroot/lib/libsndfile.so ../REGoth-Android/app/src/main/jniLibs/armeabi-v7a/libsndfile.so
+
 cd ../REGoth-Android/
 # Release build should be signed, while debug signs automatically
 ./gradlew assembleDebug
