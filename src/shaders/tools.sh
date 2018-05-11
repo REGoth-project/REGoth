b3ca91828125ff15b3d99c3e957de811135bef9d
Andre Taulien <a.taulien@live.de>
2018-05-11 19:01:31 +0200

    Fixed particle effects turning fog-colored in the distance

--- src/shaders/tools.sh
+++ src/shaders/tools.sh
@@ -11,6 +11,10 @@ vec3 computeLinearFog(float distance, float fogNear, float fogFar, vec3 color, v
 	return mix(color.rgb, fogColor, l);
 }
 
+float computeLinearFogFactor(float distance, float fogNear, float fogFar) {
+    return 1.0 - saturate((distance - fogNear)/(fogFar - fogNear));
+}
+
 vec4 applySkyColor(float intensity)
 {
 	return mix(u_SkyColors[0], u_SkyColors[1], intensity);
