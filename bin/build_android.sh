bdadf9390d52bdd5bc7df9d97f5211babc48149c
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2018-05-11 15:42:12 +0200

    Fix android build

--- bin/build_android.sh
+++ bin/build_android.sh
@@ -8,6 +8,7 @@ cd build-libsndfile
 
 cmake -DCMAKE_SYSTEM_NAME=Android -DBUILD_PROGRAMS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF \
         -DBUILD_REGTEST=OFF -DANDROID_NDK=$ANDROID_NDK_HOME -DDISABLE_EXTERNAL_LIBS=On \
+        -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a \
         -DCMAKE_INSTALL_PREFIX=$ANDROID_NDK_HOME/sysroot ../lib/libdmusic/utils/dls2sf/lib/libsndfile
 make -j4
 make install
