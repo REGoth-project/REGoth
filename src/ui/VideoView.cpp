c1dc0e1fd8dc7acfb0c7eca268ef17fa5cb942ef
Andre Taulien <a.taulien@live.de>
2018-05-20 20:28:40 +0200

    UI: Added simple Video-View

--- src/ui/VideoView.cpp
+++ src/ui/VideoView.cpp
@@ -3,3 +3,78 @@
 //
 
 #include "VideoView.h"
+#include <FfmpegVideoPlayer/VideoRead.h>
+#include <ZenLib/utils/logger.h>
+#include <render/RenderSystem.h>
+
+UI::VideoView::VideoView(Engine::BaseEngine& e) : View(e)
+{
+
+}
+
+UI::VideoView::~VideoView()
+{
+
+}
+
+void UI::VideoView::update(double dt, Engine::Input::MouseState& mstate, Render::RenderConfig& config)
+{
+    View::update(dt, mstate, config);
+
+    if(!m_QueuedVideoFile.empty())
+    {
+        playVideo(m_QueuedVideoFile, config); // Blocks until done!
+
+        m_QueuedVideoFile.clear();
+    }
+}
+
+void UI::VideoView::playVideo(const std::string& file, Render::RenderConfig& config)
+{
+    bgfx::TextureHandle videoTexture;
+    VideoRead::VideoInfo videoInfo;
+
+    VideoRead::PlayerSettings playerSettings = {
+            .onVideoInit = [&](const VideoRead::VideoInfo& info) {
+                LogInfo() << "Video resolution: " << info.width << "x" << info.height;
+
+                videoInfo = info;
+                videoTexture = bgfx::createTexture2D(info.width, info.height, false, 1, bgfx::TextureFormat::RGB8);
+            },
+
+            .onVideoFrame = [&](const VideoRead::VideoFrameData& data) {
+                LogInfo() << "New video frame ready!";
+
+                if(!bgfx::isValid(videoTexture))
+                    throw new std::runtime_error("Failed to create/write to video-texture");
+
+                bgfx::updateTexture2D(videoTexture,
+                                      0, 0, 0, 0,
+                                      (uint16_t)videoInfo.width, (uint16_t)videoInfo.height,
+                                      bgfx::copy(data.rgb.data(), (uint32_t)data.rgb.size()));
+
+                drawTexture(BGFX_VIEW, 0, 0,
+                            config.state.viewWidth, config.state.viewHeight,
+                            config.state.viewWidth, config.state.viewHeight,
+                            videoTexture,
+                            config.programs.imageProgram,
+                            config.uniforms.diffuseTexture);
+
+                bgfx::frame();
+            },
+    };
+
+    try
+    {
+        VideoRead::playVideo(file, playerSettings);
+    }
+    catch(std::runtime_error err)
+    {
+        LogError() << "Failed to play video. Reason: " << err.what();
+    }
+}
+
+void UI::VideoView::enqueueVideo(const std::string& file)
+{
+    m_QueuedVideoFile = file;
+}
--- src/ui/VideoView.h
+++ src/ui/VideoView.h
@@ -1,14 +1,43 @@
-//
-// Created by andre on 20.05.18.
-//
-
 #pragma once
+#include "View.h"
+#include <handle/HandleDef.h>
 
-
-class VideoView
+namespace UI
 {
+    class VideoView : public View
+    {
+    public:
+        VideoView(Engine::BaseEngine& e);
+        ~VideoView() override;
 
-};
+        /**
+         * Updates/draws the UI-Views
+         * @param dt time since last frame
+         * @param mstate mouse-state
+         */
+        void update(double dt, Engine::Input::MouseState& mstate, Render::RenderConfig& config) override;
 
+        /**
+         * Enqueues a video to play inside the view
+         */
+        void enqueueVideo(const std::string& file);
 
+    protected:
 
+        /**
+         * Plays a video inside the view
+         */
+        void playVideo(const std::string& file, Render::RenderConfig& config);
+
+        /**
+         * Full path of the played video file
+         */
+        std::string m_QueuedVideoFile;
+
+        /**
+         * Width and height of the given image
+         */
+        int32_t m_VideoWidth;
+        int32_t m_VideoHeight;
+    };
+}
