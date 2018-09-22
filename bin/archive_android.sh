f93d45ce35e851ac77724ffe6dcc4dcd737d3adb
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2018-09-22 19:54:38 +0200

    Change apk path

--- bin/archive_android.sh
+++ bin/archive_android.sh
@@ -7,4 +7,4 @@ if [ -z "$PLATFORM" ]; then
     PLATFORM=$TRAVIS_OS_NAME
 fi
 FILENAME=REGoth-${TRAVIS_BRANCH}-${PLATFORM}-debug.apk
-cp REGoth-Android/app/build/outputs/apk/app-arm-debug.apk dist/${FILENAME}
+cp REGoth-Android/app/build/outputs/apk/arm/debug/app-arm-debug.apk dist/${FILENAME}
