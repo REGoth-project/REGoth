0a50bcfb0c99e5148446143595fac02469acda00
Markus Obereisenbuchner <halbesbyte@googlemail.com>
2019-01-09 23:54:49 +0100

    fixed implicit fall through(s) in BspTree.cpp

--- src/engine/BspTree.cpp
+++ src/engine/BspTree.cpp
@@ -90,10 +90,14 @@ std::vector<NodeIndex> BspTree::findLeafOf(const Utils::BBox3D& bbox)
             case Utils::PLANE_INFRONT:  // Front
                 if (m_Nodes[n].front != INVALID_NODE)
                     return rec(bbox, m_Nodes[n].front);
+                else
+                    return {};
 
             case Utils::PLANE_BEHIND:  // Back
                 if (m_Nodes[n].back != INVALID_NODE)
                     return rec(bbox, m_Nodes[n].back);
+                else
+                    return {};
 
             case Utils::PLANE_SPANNING:  // Split
             {
@@ -108,8 +112,6 @@ std::vector<NodeIndex> BspTree::findLeafOf(const Utils::BBox3D& bbox)
             default:
                 return {};
         }
-
-        return {};
     };
 
     ddSetTransform(nullptr);
